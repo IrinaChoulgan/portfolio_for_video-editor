2f7fb35e7dede4cfaa1114ce0e00b16245e2f6c3
IrinaChoulgan <irina_choulgan@icloud.com>
2023-08-26 10:05:26 +0200

    fix background

--- src/18n.js
+++ src/18n.js
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'us',
+    fallbackLng: 'en',
     debug: true,
     detection: {
       order: ['localStorage', 'cookie'],
