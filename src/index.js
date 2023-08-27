37e3f4d89c0b0195857f03d465b0b14d5d1d7e9c
IrinaChoulgan <irina_choulgan@icloud.com>
2023-08-27 11:34:35 +0200

    add loader

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import './index.css';
+import { ColorRing } from  'react-loader-spinner'
 import './18n';
+
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<ColorRing
+        visible={true}
+        height="100"
+        width="100"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />}>
         <App />
     </Suspense>
 );
